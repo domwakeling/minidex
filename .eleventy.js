d33ab6ae5e55fb2fe83a586607d7827fa18d7135
Dom Wakeling <domwakeling@gmail.com>
2021-08-17 21:01:48 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,48 @@
+const beautify_html = require("js-beautify").html;
+const sass = require('dart-sass');
+const CleanCSS = require("clean-css");
+
+const codeblocks = require('@code-blocks/eleventy-plugin');
+const charts = require('@code-blocks/charts');
+
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addTransform("minimiseCSS", function (content, outputPath) {
+        if (outputPath.endsWith(".css")) {
+            return new CleanCSS({}).minify(content).styles;
+        }
+        return content;
+    })
+
+    // filter to convert SASS to CSS
+    eleventyConfig.addNunjucksFilter("convertSASS", function (value) {
+        return sass.renderSync({ data: value }).css.toString()
+    });
+
+    // filter to stringify JSON
+    eleventyConfig.addNunjucksFilter("stringify", function (value) {
+        return JSON.stringify(value);
+    });
+
+    // copy the images folder
+    eleventyConfig.addPassthroughCopy("src/images");
+
+    // charts plugin
+    eleventyConfig.addPlugin(codeblocks([charts]));
+
+    // beautify HTML files when output => ensures indentation is correct
+    eleventyConfig.addTransform("beautifyHTML", function (content, outputPath) {
+        if (outputPath.endsWith(".html")) {
+            return beautify_html(content);
+        }
+        return content;
+    });
+
+    // because we're making a function we need to return the "normal" exports object
+    return {
+        dir: {
+            input: "src",
+            output: "_site"
+        }
+    };
+};
---
+++ src/_data/minidex.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+require('dotenv').config();
+
+module.exports = async () => {
+
+    const minidexData = await axios.
+        get(`https://sheets.googleapis.com/v4/spreadsheets/${process.env.SHEET_REF}/values/Sheet1?key=${process.env.SHEET_KEY}`)
+        .then(res => res.data)
+        .then(data => {
+            return data
+                .values
+                .map(item => ({
+                    "date" : item[0] + ' ' + item[1],
+                    "dex" : item[2]
+                }))
+                .filter((item, idx) => idx !== 0)
+        })
+        .catch((err) => {
+            console.error(err);
+            return [];
+        });
+
+    const chartObj = {
+        "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+        "width": 400,
+        "height": 150,
+        "description": "Minidex over time.",
+        "data": {
+            "values": minidexData
+        },
+        "mark": {
+            "type": "area",
+            "color": "orange"
+        },
+        "encoding": {
+            "x": { "field": "date", "type": "temporal", "title": "Date", "axis": { "format": "%d %b %y" } },
+            "y": { "field": "dex", "type": "quantitative", "title": "Minidex" }
+        }
+    }
+
+    return "```vegalite\n" + JSON.stringify(chartObj) + "\n```";
+}
+
+
